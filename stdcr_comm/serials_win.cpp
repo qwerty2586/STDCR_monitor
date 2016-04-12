877c6e4ceea6ef0b16baf4366edbffad688a0f31
qwerty2586 <qwerty2@students.zcu.cz>
2016-04-12 02:06:09 +0200

    r030 (enumerace com portu win )  -ukazuji se pouze funkcni com porty

--- stdcr_comm/serials_win.cpp
+++ stdcr_comm/serials_win.cpp
@@ -1,16 +1,35 @@
 #include "serials.h"
-#include <QFileInfo>
-
+#include <windows.h>
 
 QStringList listOfAvailableSerials() {
     QStringList r;
     QString com;
+    for (int i = 1; i <= 12; ++i) {
+        HANDLE hComm;
+        std::wstring wstringPath{L"\\\\.\\COM"}; // snazime se vytvorit \\.\COM1 ale v MS string formatu
+        wstringPath.append(std::to_wstring(i));
+        const wchar_t *FileFullPath = wstringPath.c_str();
+        hComm = CreateFileW(FileFullPath,
+                            GENERIC_READ | GENERIC_WRITE,
+                            0,
+                            0,
+                            OPEN_EXISTING,
+                            FILE_FLAG_OVERLAPPED,
+                            0);
+        if (hComm != INVALID_HANDLE_VALUE) { // pokud je handle platna, tak serial existuje
+            com = "COM" + QString::number(i);
+            r.append(com);
+            CloseHandle(hComm);
+        }
+    }
 
+    /* zalozni kod v pripade ze se ukaze ze vyse udedena metoda neni spolehliva:
 
     for (int i = 1; i <= 12; i++) {
         com = "COM" + QString::number(i);
         r.append(com);
     }
+    */
 
     return r;
 };
